b9e8556ab49d1409614e67071d5d27634d829f95
Fabian Losada <fabianlosada2019@gmail.com>
2024-04-22 20:55:44 -0400

    Mi primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ElArteDeFabianMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
